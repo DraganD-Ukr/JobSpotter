d91ee367d549458811f62d1009aa3a8da48a5a64
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-02-09 17:03:08 +0000

    Fixed url of address controller in client

--- jobspotter_backend/services/job-post/src/main/java/org/job_spotter/jobpost/client/AddressServiceClient.java
+++ jobspotter_backend/services/job-post/src/main/java/org/job_spotter/jobpost/client/AddressServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "http://user-service/api/v1/job-posts")
+@FeignClient(name = "user-service", url = "http://user-service/api/v1/users/addresses")
 public interface AddressServiceClient {
 
     @GetMapping
