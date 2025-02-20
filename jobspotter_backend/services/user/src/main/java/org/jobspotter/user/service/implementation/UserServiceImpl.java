0e223cb9b12cfba0c2367c0db91c35ae418db134
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-02-20 21:45:25 +0000

    JOBSPOTTER-98: Unit Tests for user-service (BackEnd)                                    test: added unit tests for user-service refactor: removed try catch block to throw proper exceptions(handled by exception handler)

--- jobspotter_backend/services/user/src/main/java/org/jobspotter/user/service/implementation/UserServiceImpl.java
+++ jobspotter_backend/services/user/src/main/java/org/jobspotter/user/service/implementation/UserServiceImpl.java
@@ -93,14 +93,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public ResponseEntity<UserResponse> getUserById(String accessToken) {
+    public ResponseEntity<UserResponse> getUserById(String accessToken) throws Exception {
         User user;
-        try {
-            user = userRepository.findByUserId(JWTUtils.getUserIdFromToken(accessToken));
-        } catch (Exception e) {
-            log.error("Failed to get user details", e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+
+        UUID userId = JWTUtils.getUserIdFromToken(accessToken);
+        user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + userId + " not found"));
+
+
 
         return new ResponseEntity<>(UserResponse.builder()
                 .userId(user.getUserId())
