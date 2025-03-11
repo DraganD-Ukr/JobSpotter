313785a3e4c0dcd594312d7267f9f023333ff124
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-03-11 18:28:28 +0000

    feat: add MongoDB configuration and OpenAPI documentation for report service. Enhanced Report model with Swagger annotations and updated properties for API documentation.

--- jobspotter_backend/services/report/src/main/java/org/jobspotter/report/model/Report.java
+++ jobspotter_backend/services/report/src/main/java/org/jobspotter/report/model/Report.java
@@ -2,6 +2,7 @@ package org.jobspotter.report.model;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -25,27 +26,39 @@ import java.util.UUID;
 public class Report {
 
     @Id
+    @Schema(description = "Report ID with type of UUID", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private String reportId;
 
     @NotNull
+    @Schema(description = "Reporter ID type of UUID", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private UUID reporterId;
 
     @NotNull
+    @Schema(description = "Reported User ID", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private UUID reportedUserId;
 
+    @Schema(description = "Reported Job Post ID, optional", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private Long reportedJobPostId;
+
+    @Schema(description = "Reported Applicant ID, optional", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private Long reportedApplicantId;
+
+    @Schema(description = "Reported Review ID, optional", example = "662fdc3e-937b-4042-96e5-358de745695c")
     private Long reportedReviewId;
 
     @Size(min = 5, max = 150)
+    @Schema(description = "Report message", example = "This user is posting inappropriate content.")
     private String reportMessage;
 
     @Field("reportTags") // ADD @Field ANNOTATION HERE - Field name in MongoDB will be "reportTags"
+    @Schema(description = "Report tags", example = "[\"INAPPROPRIATE_CONTENT\", \"SPAM\"]")
     private Set<ReportTag> reportTags = new HashSet<>();
 
     @Field("reportStatus") // ADD @Field ANNOTATION HERE - Field name in MongoDB will be "reportStatus"
+    @Schema(description = "Report status", example = "PENDING")
     private ReportStatus reportStatus;
 
     @CreatedDate
+    @Schema(description = "Report creation date", example = "2021-10-10T10:00:00")
     private LocalDateTime createdAt;
 }
