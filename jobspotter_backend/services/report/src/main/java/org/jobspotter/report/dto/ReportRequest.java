40e0c6a574b305d22a1c125daf0af325c2c63865
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-03-11 16:35:31 +0000

    feat: enhance ReportRequest validation with additional constraints for reportedJobPostId, reportedApplicantId, and reportedReviewId

--- jobspotter_backend/services/report/src/main/java/org/jobspotter/report/dto/ReportRequest.java
+++ jobspotter_backend/services/report/src/main/java/org/jobspotter/report/dto/ReportRequest.java
@@ -1,5 +1,7 @@
 package org.jobspotter.report.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -18,12 +20,19 @@ public class ReportRequest {
 
 
     @NotNull
+    @org.hibernate.validator.constraints.UUID
     private UUID reportedUserId;
 
+    @Min(1)
+    @Max(Long.MAX_VALUE)
     private Long reportedJobPostId;
 
+    @Min(1)
+    @Max(Long.MAX_VALUE)
     private Long reportedApplicantId;
 
+    @Min(1)
+    @Max(Long.MAX_VALUE)
     private Long reportedReviewId;
 
     @Size(min = 5, max = 150)
