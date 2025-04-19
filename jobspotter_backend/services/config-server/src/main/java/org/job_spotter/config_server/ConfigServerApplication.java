c2faae28b89b3525d2655537c289210b6b343097
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-19 23:39:33 +0100

    Attempt to trigger pipeline

--- jobspotter_backend/services/config-server/src/main/java/org/job_spotter/config_server/ConfigServerApplication.java
+++ jobspotter_backend/services/config-server/src/main/java/org/job_spotter/config_server/ConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//Main class for the Spring
+//Main class for the Spring Boot
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
