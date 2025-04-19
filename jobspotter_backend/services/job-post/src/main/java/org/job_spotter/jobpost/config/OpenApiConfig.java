aaef87cd1a6ae9a406965f6c89f0c28dc4e30c07
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-19 20:06:01 +0100

    feat: Remove hardcoded server URLs from OpenAPI configuration and prepare for dynamic server configuration

--- jobspotter_backend/services/job-post/src/main/java/org/job_spotter/jobpost/config/OpenApiConfig.java
+++ jobspotter_backend/services/job-post/src/main/java/org/job_spotter/jobpost/config/OpenApiConfig.java
@@ -3,11 +3,8 @@ package org.job_spotter.jobpost.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
@@ -16,7 +13,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(List.of(new Server().url("http://localhost:8081")))
                 .info(new Info()
                         .title("Job Post Service API")
                         .version("1.0.0")
--- jobspotter_backend/services/report/src/main/java/org/jobspotter/report/config/OpenApiConfig.java
+++ jobspotter_backend/services/report/src/main/java/org/jobspotter/report/config/OpenApiConfig.java
@@ -3,11 +3,8 @@ package org.jobspotter.report.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
@@ -16,7 +13,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(List.of(new Server().url("http://localhost:8084")))
                 .info(new Info()
                         .title("Report Service API")
                         .version("0.0.1")
