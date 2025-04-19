438822cfb47cf56694307e6338276fda71a1dd98
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-19 23:49:59 +0100

    Attempt to trigger pipeline for report-service

--- jobspotter_backend/services/report/src/main/java/org/jobspotter/report/ReportApplication.java
+++ jobspotter_backend/services/report/src/main/java/org/jobspotter/report/ReportApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// Main class for the Spring
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ReportApplication {
