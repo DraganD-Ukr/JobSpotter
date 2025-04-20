17cf37cab4276972934df353b64347a8f17089fe
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-20 21:07:41 +0100

    refactor: enhance consumerFactory method to use bootstrap address and improve deserialization settings

--- jobspotter_backend/services/notification/src/main/java/org/jobspotter/notification/config/KafkaConsumerConfig.java
+++ jobspotter_backend/services/notification/src/main/java/org/jobspotter/notification/config/KafkaConsumerConfig.java
@@ -2,7 +2,10 @@ package org.jobspotter.notification.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.jobspotter.notification.model.Notification;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.ssl.DefaultSslBundleRegistry;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +16,10 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import java.util.HashMap;
 import java.util.Map;
 @Configuration
 @RequiredArgsConstructor
@@ -21,11 +27,22 @@ public class KafkaConsumerConfig {
 
     private final KafkaProperties kafkaProperties;
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
 
     @Bean
     public ConsumerFactory<String, Notification> consumerFactory() {
-        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties(new DefaultSslBundleRegistry());
-        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset().toString().toLowerCase());
+        props.put(StringDeserializer.class.getName(), StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Notification.class);
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
