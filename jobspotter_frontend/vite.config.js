1544ba66088bd135c01e9be4e8750b8bc804b6a0
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-23 21:00:29 +0100

    feat: Enhance Vite configuration to load environment variables for dynamic API URL

--- jobspotter_frontend/vite.config.js
+++ jobspotter_frontend/vite.config.js
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8100',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env variables based on the current mode (dev or prod)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,  // Use the loaded API URL from environment variables
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),  // You can use this to inject the environment into your code
+    },
+  }
 })
