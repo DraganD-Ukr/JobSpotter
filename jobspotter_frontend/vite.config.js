15cbd59104624424786c1ddc852a24adfcaf48b5
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-04-24 12:44:07 +0100

    fix: correct API destination path in Vercel configuration and adjust rewrite rule in Vite setup

--- jobspotter_frontend/vite.config.js
+++ jobspotter_frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
         '/api': {
           target: env.VITE_API_URL,  // Use the loaded API URL from environment variables
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          rewrite: (path) => path,
           secure: true,
         },
       },
